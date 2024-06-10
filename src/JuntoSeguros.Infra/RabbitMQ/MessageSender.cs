1021bd2a4b5c6a5a72aa8eec73a475d6f9eca7d3
Thiago Cardoso Barbosa <thiagocbcunha@gmail.com>
2024-06-09 22:36:41 -0300

    readme update

--- src/JuntoSeguros.Infra/RabbitMQ/MessageSender.cs
+++ src/JuntoSeguros.Infra/RabbitMQ/MessageSender.cs
@@ -1,12 +1,13 @@
-﻿using JuntoSeguros.Domain.Contracts;
-using MassTransit;
+﻿using MassTransit;
+using JuntoSeguros.Domain.Contracts;
 
 namespace JuntoSeguros.Infra.RabbitMQ;
 
-public class MessageSender(IBus _bus) : IMessagingSender
+public class MessageSender(IBus _bus, Enterprise.Library.Contracts.IActivityFactory _activityFactory) : IMessagingSender
 {
     public async Task Send<TMessage>(TMessage message) where TMessage : class
     {
+        _activityFactory.Start("MessageSender - New Event");
         var sendEndpoint = await _bus.GetSendEndpoint(new Uri($"queue:JuntoSeguros.{typeof(TMessage).Name}.Event"));
         await sendEndpoint.Send(message);
     }
